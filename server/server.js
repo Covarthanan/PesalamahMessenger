b05b6c9e777fe6475b057604f59c8c29cf99bd7a
Covarthanan.K <covarthanan.k@volantetech.net>
2020-11-27 19:11:11 +0530

    First Commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const { constants } = require('buffer');
+var express = require('express');
+var app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+var app_path = '../dist/Pesalamah';
+
+app.use('/',express.static(path.join(__dirname,app_path)))
+.get('*',(req,res)=> res.sendFile(path.join(__dirname,app_path+'/index.html')))
+.listen(PORT,()=>console.log(`Listening On ${PORT}`));
