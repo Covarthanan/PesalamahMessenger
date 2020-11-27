a2c3bacebf8bb6236be725ecb9c2886b6010be9b
Covarthanan <covart1196@gmail.com>
2020-11-27 19:26:50 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyD1CxGgqS02cHgZlyvXg5yEezDvYymxrbk",
+    authDomain: "myfirstproject-e04bb.firebaseapp.com",
+    databaseURL: "https://myfirstproject-e04bb.firebaseio.com",
+    projectId: "myfirstproject-e04bb",
+    storageBucket: "myfirstproject-e04bb.appspot.com",
+    messagingSenderId: "130255543307",
+    appId: "1:130255543307:web:ad797ced470604c330bb0e",
+    measurementId: "G-NSEM86LP9M"
+   }
 };
