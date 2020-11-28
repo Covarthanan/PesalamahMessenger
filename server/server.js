d54a3837acac91c3c456e9539dad38af8ab0cc4f
Covarthanan <covart1196@gmail.com>
2020-11-28 08:28:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-var app_path = '../dist/Pesalamah';
+var app_path = '../dist/pesalamah';
 
 app.use('/',express.static(path.join(__dirname,app_path)))
 .get('*',(req,res)=> res.sendFile(path.join(__dirname,app_path+'/index.html')))
